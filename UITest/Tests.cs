b559954284528b45909cf6e1bc65a5b1718a2d56
Ione Souza Junior <junior@ionixjunior.com.br>
2016-06-27 20:45:25 -0300

    Implementado testes do CRUD básico

--- UITest/Tests.cs
+++ UITest/Tests.cs
@@ -25,10 +25,58 @@ namespace UITest
 			app = AppInitializer.StartApp(platform);
 		}
 
+		//[Test]
+		//public void AppLaunches()
+		//{
+		//	app.Screenshot("First screen.");
+		//}
+
 		[Test]
-		public void AppLaunches()
+		public void BasicFlow()
 		{
-			app.Screenshot("First screen.");
+			string firstName = "John";
+			string lastName = "Smith";
+			string email = "john@smith.com";
+
+			// Cadastro
+			app.Tap(e => e.Id("tbiAdd"));
+
+			app.Tap(e => e.Text("Nome"));
+			app.EnterText(firstName);
+
+			app.Tap(e => e.Text("Sobrenome"));
+			app.EnterText(lastName);
+
+			app.Tap(e => e.Text("Email"));
+			app.EnterText(email);
+
+			app.Tap(e => e.Id("tbiSave"));
+
+
+			// Edição
+			app.Tap(e => e.Text(firstName));
+
+			firstName = "Sr. John";
+			app.Tap(e => e.Text("Nome"));
+			app.ClearText();
+			app.EnterText(firstName);
+
+			lastName = "A. Smith";
+			app.Tap(e => e.Text("Sobrenome"));
+			app.ClearText();
+			app.EnterText(lastName);
+
+			email = "john@asmith.com";
+			app.Tap(e => e.Text("Email"));
+			app.ClearText();
+			app.EnterText(email);
+			app.Tap(e => e.Id("tbiSave"));
+
+
+			// Exclusão
+			app.Tap(e => e.Text(firstName));
+
+			app.Tap(e => e.Id("btnDelete"));
 		}
 	}
 }
