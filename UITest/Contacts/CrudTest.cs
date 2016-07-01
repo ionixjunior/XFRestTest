5015d8ea375b378aed6e75c6ff7f0fe30e158c77
Ione Souza Junior <junior@ionixjunior.com.br>
2016-06-30 21:39:45 -0300

    Ajustado testes

--- UITest/Contacts/CrudTest.cs
+++ UITest/Contacts/CrudTest.cs
@@ -12,50 +12,47 @@ namespace UITest.Contacts
 		[Test]
 		public void BasicFlow()
 		{
-			//string firstName = "John";
-			//string lastName = "Smith";
-			//string email = "john@smith.com";
+			string firstName = "John";
+			string lastName = "Smith";
+			string email = "john@smith.com";
 
-			app.Repl();
+			// Cadastro
+			//app.Tap(e => e.Id("tbiAdd"));//problemas no android
+			app.Tap(e => e.Text("Adicionar"));
 
+			app.EnterText(e => e.Marked("etrFirstName"), firstName);
+			app.EnterText(e => e.Marked("etrLastName"), lastName);
+			app.EnterText(e => e.Marked("etrEmail"), email);
 
-			//// Cadastro
-			////app.Tap(e => e.Id("tbiAdd"));//problemas no android
-			//app.Tap(e => e.Text("Adicionar"));
-
-			//app.EnterText(e => e.Marked("etrFirstName"), firstName);
-			//app.EnterText(e => e.Marked("etrLastName"), lastName);
-			//app.EnterText(e => e.Marked("etrEmail"), email);
-
-			////app.Tap(e => e.Id("tbiSave"));//problemas no android
-			//app.Tap(e => e.Text("Salvar"));
+			//app.Tap(e => e.Id("tbiSave"));//problemas no android
+			app.Tap(e => e.Text("Salvar"));
 
 
-			//// Edição
-			//app.Tap(e => e.Text(firstName));
+			// Edição
+			app.Tap(e => e.Text(firstName));
 
-			//firstName = "Sr. John";
-			//app.Tap(e => e.Marked("etrFirstName"));
-			//app.ClearText();
-			//app.EnterText(firstName);
+			firstName = "Sr. John";
+			app.Tap(e => e.Marked("etrFirstName"));
+			app.ClearText();
+			app.EnterText(firstName);
 
-			//lastName = "A. Smith";
-			//app.Tap(e => e.Marked("etrLastName"));
-			//app.ClearText();
-			//app.EnterText(lastName);
+			lastName = "A. Smith";
+			app.Tap(e => e.Marked("etrLastName"));
+			app.ClearText();
+			app.EnterText(lastName);
 
-			//email = "john@asmith.com";
-			//app.Tap(e => e.Marked("etrEmail"));
-			//app.ClearText();
-			//app.EnterText(email);
-			//app.Tap(e => e.Text("Salvar"));
+			email = "john@asmith.com";
+			app.Tap(e => e.Marked("etrEmail"));
+			app.ClearText();
+			app.EnterText(email);
+			app.Tap(e => e.Text("Salvar"));
 
 
-			//// Exclusão
-			//app.Tap(e => e.Text(firstName));
+			// Exclusão
+			app.Tap(e => e.Text(firstName));
 
-			////app.Tap(e => e.Id("btnDelete"));//problemas no android
-			//app.Tap(e => e.Text("Apagar"));
+			//app.Tap(e => e.Id("btnDelete"));//problemas no android
+			app.Tap(e => e.Text("Apagar"));
 		}
 	}
 }
